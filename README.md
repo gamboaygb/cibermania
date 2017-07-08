33de264fae23ddce95f3ead0137f1ccc7c5a0f44
root <yusmel@animaventures.com>
2017-07-08 12:38:08 +0200

    proyecto entero de cibermania

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cibermania
+cibermania
+==========
+
+A Symfony project created on July 8, 2017, 10:24 am.
---
+++ var/cache/dev/twig/66/66697dcdfbee4a9d0f1b7651c5d33412344e0288581bdbfbf11b1ede737e67df.php
@@ -0,0 +1,54 @@
+<?php
+
+/* @WebProfiler/Icon/cache.svg */
+class __TwigTemplate_814a3905ad17f9150b597779cedbcd429cbb56b96f2f6b138dd984211db8d5c0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_aea461ebd30da30f9bf6b743006cb3b50071d34285511abd31673822288690c3 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_aea461ebd30da30f9bf6b743006cb3b50071d34285511abd31673822288690c3->enter($__internal_aea461ebd30da30f9bf6b743006cb3b50071d34285511abd31673822288690c3_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Icon/cache.svg"));
+
+        $__internal_92a6e6e72c2e206b4db0c5b1c5cc85d3af8e8313c8093c8e75a4e43f939a34f1 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_92a6e6e72c2e206b4db0c5b1c5cc85d3af8e8313c8093c8e75a4e43f939a34f1->enter($__internal_92a6e6e72c2e206b4db0c5b1c5cc85d3af8e8313c8093c8e75a4e43f939a34f1_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Icon/cache.svg"));
+
+        // line 1
+        echo "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+<path fill=\"#AAA\" d=\"M2.26 6.09l9.06-4.67a1.49 1.49 0 0 1 1.37 0l9.06 4.67a1.49 1.49 0 0 1 0 2.65l-9.06 4.67a1.49 1.49 0 0 1-1.37 0L2.26 8.74a1.49 1.49 0 0 1 0-2.65zM20.55 11L12 15.39 3.45 11a1.36 1.36 0 0 0-1.25 2.42l9.17 4.73a1.36 1.36 0 0 0 1.25 0l9.17-4.73A1.36 1.36 0 0 0 20.55 11zm0 4.47L12 19.86l-8.55-4.41a1.36 1.36 0 0 0-1.25 2.42l9.17 4.73a1.36 1.36 0 0 0 1.25 0l9.17-4.73a1.36 1.36 0 0 0-1.25-2.42z\"/>
+</svg>
+";
+        
+        $__internal_aea461ebd30da30f9bf6b743006cb3b50071d34285511abd31673822288690c3->leave($__internal_aea461ebd30da30f9bf6b743006cb3b50071d34285511abd31673822288690c3_prof);
+
+        
+        $__internal_92a6e6e72c2e206b4db0c5b1c5cc85d3af8e8313c8093c8e75a4e43f939a34f1->leave($__internal_92a6e6e72c2e206b4db0c5b1c5cc85d3af8e8313c8093c8e75a4e43f939a34f1_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Icon/cache.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  25 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+<path fill=\"#AAA\" d=\"M2.26 6.09l9.06-4.67a1.49 1.49 0 0 1 1.37 0l9.06 4.67a1.49 1.49 0 0 1 0 2.65l-9.06 4.67a1.49 1.49 0 0 1-1.37 0L2.26 8.74a1.49 1.49 0 0 1 0-2.65zM20.55 11L12 15.39 3.45 11a1.36 1.36 0 0 0-1.25 2.42l9.17 4.73a1.36 1.36 0 0 0 1.25 0l9.17-4.73A1.36 1.36 0 0 0 20.55 11zm0 4.47L12 19.86l-8.55-4.41a1.36 1.36 0 0 0-1.25 2.42l9.17 4.73a1.36 1.36 0 0 0 1.25 0l9.17-4.73a1.36 1.36 0 0 0-1.25-2.42z\"/>
+</svg>
+", "@WebProfiler/Icon/cache.svg", "/var/www/cibermania/vendor/symfony/symfony/src/Symfony/Bundle/WebProfilerBundle/Resources/views/Icon/cache.svg");
+    }
+}
