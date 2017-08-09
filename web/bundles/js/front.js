51e0872e840d5dc9363217629613ce07a41fa8b5
root <root@cibermania.es>
2017-08-09 06:05:15 +0000

    ultimas modificaciones en el popup

--- web/bundles/js/front.js
+++ web/bundles/js/front.js
@@ -28,6 +28,11 @@
 
    /*animate post*/
 
+    $('.modal').css({'background':'rgba(0, 0, 0, 0.6)','opacity':'1'}).show();
+    $('.btn-success').click(function(){
+	$('.modal').hide(500);
+    });
+
     $(window).ready(function() {
         $('.post-module').hover(function() {
             $(this).find('.description').stop().animate({
