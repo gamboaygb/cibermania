007d482b0697a945728afaeb5839897e5d60ae6b
Gamboita <soportecibermania@gmail.com>
2017-08-11 23:50:21 +0200

    hidding popup

--- web/bundles/js/front.js
+++ web/bundles/js/front.js
@@ -28,10 +28,10 @@
 
    /*animate post*/
 
-    $('.modal').css({'background':'rgba(0, 0, 0, 0.6)','opacity':'1'}).show();
+ /*   $('.modal').css({'background':'rgba(0, 0, 0, 0.6)','opacity':'1'}).show();
     $('.btn-success').click(function(){
 	$('.modal').hide(500);
-    });
+    });*/
 
     $(window).ready(function() {
         $('.post-module').hover(function() {
